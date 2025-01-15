929f819fe6b93f16160cdf021d340f45b251ab86
SrMarkLau <marcoslauande@hotmail.com>
2025-01-15 12:11:18 -0300

    Update simple.py

--- simple.py
+++ simple.py
@@ -2,15 +2,11 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class HelloWorldHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # Envia o status HTTP 200 (OK)
         self.send_response(200)
-        # Define o tipo de conteúdo como texto simples
         self.send_header("Content-Type", "text/plain")
         self.end_headers()
-        # Responde com "Hello, World!"
         self.wfile.write(b"Hello, World!")
 
-# Porta 80 exige privilégios de administrador em muitos sistemas
 def run(server_class=HTTPServer, handler_class=HelloWorldHandler, port=80):
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
